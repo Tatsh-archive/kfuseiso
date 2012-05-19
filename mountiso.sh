f35708b8a2551bda789f50d11c410d17435e37e2
Tatsh <ddrtist@gmail.com>
2012-05-19 13:35:05 -0500

    Fix variable

--- mountiso.sh
+++ mountiso.sh
@@ -6,14 +6,14 @@ cd "$DIR"
 [[ "$1" = "" ]] && kdialog --error "No ISO file specified." && exit 1
 
 LASTPATH=""
-if [[ -f ~/.kisomount-last ]]; then
-	LASTPATH=$(<~/.kisomount-last)
+if [[ -f ~/.kfuseisomount-last ]]; then
+	LASTPATH=$(<~/.kfuseisomount-last)
 fi
 
 LASTPATH="$(kdialog --inputbox "Mount point" "$LASTPATH")"
 
 # User hit cancel
-[[ "$1" = "" ]] && exit 1
+[[ "$LASTPATH" = "" ]] && exit 1
 
 if [[ ! -d "$LASTPATH" ]]; then
 	kdialog --error "There is no directory named '$LASTPATH'."
@@ -25,5 +25,5 @@ fi
 fuseiso "$1" "$LASTPATH" 2> ~/.kisomount-log
 (($? != 0)) && kdialog -error "There was an error mounting." && exit 1
 
-echo "$LASTPATH" > ~/.kisomount-last
-rm -f ~/.kisomount-log
+echo "$LASTPATH" > ~/.kfuseisomount-last
+rm -f ~/.kfuseisomount-last
